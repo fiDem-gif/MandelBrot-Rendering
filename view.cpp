4c7164ba65b8f9aa1d3edcae45a293bfa07f66e3
Amit Pal <amitpal040402006@gmail.com>
2026-08-09 23:57:28 +0530

    Visualizing Mandelbrot onto the SDL Screen

--- view.cpp
+++ view.cpp
@@ -1,67 +1,93 @@
-#define SDL_MAIN_HANDLED
-#include <SDL2/SDL.h>
+    #define SDL_MAIN_HANDLED
+    #include <SDL2/SDL.h>
+    #include <complex>
+    #include <cmath>
 
-#define width 900
-#define height 600
+    #define width 900
+    #define height 600
+    #define COLOR_WHITE 0xFFFFFFFF
+    #define MANDEL_LIMIT 1000
 
-int main (){
-    // Window or canvas frame initialization and attributes
-    SDL_Window  *window = SDL_CreateWindow(
-        "MandelBrot Set",
-        SDL_WINDOWPOS_CENTERED ,
-        SDL_WINDOWPOS_CENTERED,
-        width ,
-        height ,
-        0 );
+    int max_iterations = 50;
+    
+    int check_4_mandelbrot(double real, double img, int max_iterations)
+    {
+        std::complex<double> c(real, img);
+        std::complex<double> z(0,0);
         
-    // Renderer initialization 
-    SDL_Renderer *renderer = SDL_CreateRenderer(
-        window,
-        -1,
-        SDL_RENDERER_ACCELERATED);
-
-
-    // Window error handling
-    if(!window){
-        SDL_Log("Failed to create window: %s", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
-
-    // Renderer error handling
-    if(!renderer){
-        SDL_Log("Renderer error: %s", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
-    int application_running = 1 ;
-    while (application_running){
-        SDL_Event event;
-
-        while(SDL_PollEvent(&event)){
-            if (event.type == SDL_QUIT) {
-                application_running = 0;
-            }
-
-            
-
+        for (int i =0; i < max_iterations; i++)
+        {
+            z = pow(z,2) + c;
         }
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    SDL_RenderPresent(renderer);
-    SDL_Delay(16);
-
-    
-
+        double real_part = z.real();
+        double img_part = z.imag();
+        return abs(z) < MANDEL_LIMIT;
     }
 
-    SDL_DestroyRenderer(renderer);
-    SDL_Quit();
+    void visualize_mandelbrot(SDL_Surface *psurface)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0 ; y < height; y++)
+            {
+                
+                double real = -2.0 + ((double)x / width) * 3.0;
+                double img = -1.0 + ((double)y / height) * 2.0;
+                
+                if ( check_4_mandelbrot((double) real , (double) img, max_iterations))
+                {
+                    SDL_Rect rect = {x,y, 1, 1};
+                    SDL_FillRect(psurface, &rect, COLOR_WHITE);
+                }
+            }
+        }
+    }
 
-    return 0;
-}
+    int main (){
+
+        if (SDL_Init(SDL_INIT_VIDEO) != 0) 
+        {
+            SDL_Log("SDL Init Error: %s", SDL_GetError());
+            return 1;
+        }
+        // Window or canvas frame initialization and attributes
+        SDL_Window  *window = SDL_CreateWindow(
+            "MandelBrot Set",
+            SDL_WINDOWPOS_CENTERED ,
+            SDL_WINDOWPOS_CENTERED,
+            width ,
+            height ,
+            0 );
+                
+            
+        
+        if(!window){
+            SDL_Log("Failed to create window: %s", SDL_GetError());
+            SDL_Quit();
+            return 1;
+        }
+        
+        
+        
+        SDL_Surface *psurface = SDL_GetWindowSurface(window);
+        visualize_mandelbrot(psurface);
+        
+        int application_running = 1 ;
+        while (application_running){
+            SDL_Event event;
+
+            while(SDL_PollEvent(&event)){
+                if (event.type == SDL_QUIT) {
+                    application_running = 0;
+                }
+                SDL_UpdateWindowSurface(window);
+
+                SDL_Delay(16);
+                
+                
+                
+            }    
+        }
+
+    }
